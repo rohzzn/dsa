6a6fa2b7dd78ea2e2a5d45643001db64ec5daea2
Rohan <rohansanjeev6464@gmail.com>
2025-01-01 18:41:22 -0500

    Remove global styles and layout components; integrate metadata in _app.tsx for improved SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
+// src/pages/_app.tsx
+
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/layout/Layout'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>DSA</title> {/* Replace with your desired title */}
+        <meta name="description" content="Learn Data Structures and Algorithms" />
+        <link rel="icon" href="/favicon.png" /> {/* Ensure the path is correct */}
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
