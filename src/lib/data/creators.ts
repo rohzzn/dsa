7d2744074174db6f1bdc7a5186eebcc72640d87b
Rohan <rohansanjeev6464@gmail.com>
2025-01-01 13:10:35 -0500

    Add creators data and update package dependencies

---
+++ src/lib/data/creators.ts
@@ -0,0 +1,23 @@
+export const creatorsData = [
+    {
+      name: 'Abdul Bari',
+      platform: 'YouTube',
+      topics: ['Algorithms', 'Data Structures', 'Theory'],
+      description: 'Clear explanations of complex algorithms with visual aids',
+      url: 'https://www.youtube.com/channel/UCZCFT11CWBi3MHNlGf019nw'
+    },
+    {
+      name: 'William Fiset',
+      platform: 'GitHub',
+      topics: ['Data Structures', 'Implementation', 'Code'],
+      description: 'Detailed implementations with explanations',
+      url: 'https://github.com/williamfiset'
+    },
+    {
+      name: 'GeeksforGeeks',
+      platform: 'Website',
+      topics: ['DSA', 'Practice', 'Interview Prep'],
+      description: 'Comprehensive DSA coverage with practice problems',
+      url: 'https://www.geeksforgeeks.org'
+    }
+  ]
